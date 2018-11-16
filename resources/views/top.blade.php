c3c6baa84ba87f4ad16837d7e0c76ea56726844c
Tomoya Koyanagi <tomk79@gmail.com>
2018-11-16 15:33:35 +0900

    top画面のようこそメッセージを修正

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -8,15 +8,7 @@
 <div class="container">
     <h1>{{ $title }}</h1>
     <p>
-        {{ __('My practice for basic CRUD of Laravel 5.7.') }}
+        {{ $title }} へようこそ。<br />
     </p>
-    <ul>
-        <li>
-            GitHub:
-            <a href="https://github.com/pickles2/app-burdock" target="_blank">
-                https://github.com/pickles2/app-burdock
-            </a>
-        </li>
-    </ul>
 </div>
 @endsection
