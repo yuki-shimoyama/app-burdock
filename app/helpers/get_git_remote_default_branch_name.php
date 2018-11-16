36b9d3e33187bb7bf6018ae0fac7812d583356c3
Tomoya Koyanagi <tomk79@gmail.com>
2018-11-16 20:02:15 +0900

    get_git_remote_default_branch_name() : リモートサーバーのデフォルトブランチ名を取得するようになった

--- app/helpers/get_git_remote_default_branch_name.php
+++ app/helpers/get_git_remote_default_branch_name.php
@@ -3,6 +3,18 @@
  * Gitリモートサーバーからデフォルトのブランチ名を取得する
  */
 function get_git_remote_default_branch_name( $git_url = null ) {
-    $branch_name = 'master'; // TODO: 仮実装
-    return $branch_name;
+    $default = 'master';
+    if( !strlen( $git_url ) ){
+        return $default;
+    }
+    $result = shell_exec('git ls-remote --symref '.escapeshellarg($git_url).' HEAD');
+    if(!is_string($result) || !strlen($result)){
+        return $default;
+    }
+
+    if( !preg_match('/^ref\: refs\/heads\/([^\s]+)\s+HEAD/', $result, $matched) ){
+        return $default;
+    }
+
+    return $matched[1];
 }
