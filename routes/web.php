f98b66d33c55748423038583f8ee10cb509ccc79
yuki.shimoyama <yuki.shimoyama@imjp.co.jp>
2018-12-10 19:19:35 +0900

    コンテンツのホーム画面を追加

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::delete('projects/{project}/{branch_name}', 'ProjectController@destroy');
 
 Route::post('upload/{project}/{branch_name}', 'SitemapController@upload');
 Route::post('download/{project}/{branch_name}', 'SitemapController@download');
-Route::post('publish/{project}/{branch_name}', 'PublishController@publish');
+Route::get('publish/{project}/{branch_name}', 'PublishController@publish');
 
 Route::get('pages/{project}/{branch_name}/index.html', 'PageController@index');
 Route::get('pages/{project}/{branch_name}', 'PageController@show');
