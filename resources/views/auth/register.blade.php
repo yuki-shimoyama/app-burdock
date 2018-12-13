f849c7eda5799c12900752dacaf207baaabe7a1c
yuki.shimoyama <yuki.shimoyama@imjp.co.jp>
2018-12-13 12:29:53 +0900

    register画面のコンテナを中央寄せ

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-md-offset-2">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
