3ed02815b9e32efab29ede2dd24be56087deb3c5
yuki.shimoyama <yuki.shimoyama@imjp.co.jp>
2018-12-14 16:43:43 +0900

    トップ画面にWYSIWYGエディタを追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -10,5 +10,17 @@
     <p>
         {{ $title }} へようこそ。<br />
     </p>
+    <script type="text/javascript" src="/js/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector : 'textarea',
+            plugins  : 'jbimages link autolink preview',
+            toolbar  : 'bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link jbimages | preview',
+            menubar  : false,
+            relative_urls : false,
+            language : 'ja'
+        });
+    </script>
+    <textarea name="hoge" style="height: 650px;"></textarea>
 </div>
 @endsection
